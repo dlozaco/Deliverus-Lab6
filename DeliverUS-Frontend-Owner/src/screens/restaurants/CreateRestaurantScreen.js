88d80dac222699b3fc5e6c0e74ce4c5299c70848
dlozaco975@gmail.com <dlozaco975@gmail.com>
2025-04-03 11:43:39 +0200

    Create restaurant screen

--- DeliverUS-Frontend-Owner/src/screens/restaurants/CreateRestaurantScreen.js
+++ DeliverUS-Frontend-Owner/src/screens/restaurants/CreateRestaurantScreen.js
@@ -11,11 +11,163 @@ import * as GlobalStyles from '../../styles/GlobalStyles'
 import restaurantLogo from '../../../assets/restaurantLogo.jpeg'
 import restaurantBackground from '../../../assets/restaurantBackground.jpeg'
 import { showMessage } from 'react-native-flash-message'
-import { ErrorMessage, Formik } from 'formik'
+import { ErrorMessage, Form, Formik } from 'formik'
 import TextError from '../../components/TextError'
 
-export default function CreateRestaurantScreen({ navigation }) {
+export default function CreateRestaurantScreen () {
+  const initialRestuarantValues = {
+    name: null,
+    description: null,
+    address: null,
+    postalCode: null,
+    url: null,
+    shippingCosts: null,
+    email: null,
+    phone: null
+  }
 
+  const pickImage = async (onSuccess) => {
+    const result = await ExpoImagePicker.launchImageLibraryAsync({
+      mediaTypes: ExpoImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1
+    })
+    if (!result.canceled) {
+      if (onSuccess) {
+        onSuccess(result)
+      }
+    }
+  }
 
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS !== 'web') {
+        const { status } = await ExpoImagePicker.requestMediaLibraryPermissionsAsync()
+        if (status !== 'granted') {
+          alert('Sorry, we need camera roll permissions to make this work!')
+        }
+      }
+    })()
+  }, [])
 
+  return (
+    <Formik
+      initialValues={initialRestuarantValues}
+    >
+      {({ setFieldValue, values }) => (
+        <ScrollView>
+          <View style={{ alignItems: 'center' }}>
+        <View style={{ width: '60%' }}>
+        <InputItem
+          name='name'
+          label='Name:'
+        />
+        <InputItem
+          name='description'
+          label='Description:'
+        />
+        <InputItem
+          name='address'
+          label='Address:'
+        />
+        <InputItem
+          name='postalCode'
+          label='Postal Code:'
+        />
+        <InputItem
+          name='url'
+          label='Url:'
+        />
+        <InputItem
+          name='shippingCosts'
+          label='Shipping Costs:'
+        />
+        <InputItem
+          name='email'
+          label='Email:'
+        />
+        <InputItem
+          name='phone'
+          label='Phone:'
+        />
+        <Pressable
+          onPress={() =>
+            pickImage(
+              async result => {
+                await setFieldValue('logo', result)
+              }
+            )
+          }
+        >
+          <TextRegular>Logo: </TextRegular>
+          <Image
+            style={styles.image}
+            source={values.logo ? { uri: values.logo.assets[0].uri } : restaurantLogo}
+          />
+        </Pressable>
+
+        <Pressable onPress={() =>
+          pickImage(
+            async result => {
+              await setFieldValue('heroImage', result)
+            }
+          )
+        }
+          style={styles.imagePicker}
+        >
+          <TextRegular>Hero image: </TextRegular>
+          <Image style={styles.image} source={values.heroImage ? { uri: values.heroImage.assets[0].uri } : restaurantBackground} />
+        </Pressable>
+
+        <Pressable
+          onPress={() => console.log('Button pressed')
+          }
+          style={({ pressed }) => [
+            {
+              backgroundColor: pressed
+                ? GlobalStyles.brandPrimaryTap
+                : GlobalStyles.brandPrimary
+            },
+            styles.button
+          ]}>
+          <TextRegular textStyle={styles.text}>
+            Create restaurant
+          </TextRegular>
+        </Pressable>
+        </View>
+      </View>
+        </ScrollView>
+      )}
+    </Formik>
+  )
 }
+
+const styles = StyleSheet.create({
+  button: {
+    borderRadius: 8,
+    height: 40,
+    padding: 10,
+    width: '100%',
+    marginTop: 20,
+    marginBottom: 20
+  },
+  text: {
+    fontSize: 16,
+    color: GlobalStyles.brandSecondary,
+    textAlign: 'center'
+  },
+  imagePicker: {
+    height: 40,
+    paddingLeft: 10,
+    marginTop: 20,
+    marginBottom: 80
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderWidth: 1,
+    alignSelf: 'center',
+    marginTop: 5
+  }
+})
